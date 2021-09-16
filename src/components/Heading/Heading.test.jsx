0d3dfd7c467a838b43fd096b8904acf3cb558e4b
Dan Sobr <d.wiklich@gmail.com>
2021-09-15 23:44:12 -0300

    Created snapshot to component

--- src/components/Heading/Heading.test.jsx
+++ src/components/Heading/Heading.test.jsx
@@ -89,4 +89,9 @@ describe('<Heading />', () => {
 
     expect(h6.tagName.toLowerCase()).toBe('h6');
   });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(<Heading>texto</Heading>);
+    expect(container).toMatchSnapshot();
+  });
 });
