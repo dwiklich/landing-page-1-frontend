b81184a984a64a76cfcb213e1a52276a1f537408
Dan Sobr <d.wiklich@gmail.com>
2021-10-29 18:51:36 -0300

    Updated map-data and tests, added func section and func menu of mapData

--- src/api/map-data.test.js
+++ src/api/map-data.test.js
@@ -7,7 +7,13 @@ describe('map-data', () => {
     expect(pagesData.slug).toBe('');
     expect(pagesData.title).toBe('');
     expect(pagesData.sections).toEqual([]);
-    expect(pagesData.menu).toEqual({});
+    expect(pagesData.menu).toEqual({
+      link: '',
+      links: [],
+      newTab: false,
+      srcImg: '',
+      text: '',
+    });
   });
 
   it('should map data if there are data', () => {
@@ -17,13 +23,51 @@ describe('map-data', () => {
         slug: 'slug',
         title: 'title',
         sections: [1, 2, 4],
-        menu: { menu: 'menutexto' },
+        menu: {
+          open_in_new_tab: false,
+          _id: '602fdf30540c00269e0561ae',
+          logo_text: 'Landing Page',
+          logo_link: '#home',
+          menu: [
+            {
+              open_in_new_tab: false,
+              link_text: 'intro',
+              url: '#intro',
+            },
+            {
+              open_in_new_tab: false,
+              link_text: 'grid-one',
+              url: '#grid-one',
+            },
+          ],
+          logo: {
+            alternativeText: 'Landing Page Logo',
+            url: 'a.svg',
+          },
+        },
       },
     ])[0];
     expect(pagesData.footerHtml).toBe('<p>Hello</p>');
     expect(pagesData.slug).toBe('slug');
     expect(pagesData.title).toBe('title');
     expect(pagesData.sections).toEqual([1, 2, 4]);
-    expect(pagesData.menu).toEqual({ menu: 'menutexto' });
+    expect(pagesData.menu).toEqual({
+      link: '#home',
+      links: [
+        {
+          children: 'intro',
+          link: '#intro',
+          newTab: false,
+        },
+        {
+          children: 'grid-one',
+          link: '#grid-one',
+          newTab: false,
+        },
+      ],
+      newTab: false,
+      srcImg: 'a.svg',
+      text: 'Landing Page',
+    });
   });
 });
