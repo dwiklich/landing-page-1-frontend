10115a35c494df0907ca14867817942a35bac195
Daniel Sobrinho <dev.sobrinho@gmail.com>
2022-01-21 19:16:30 -0300

    Create file redirect

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,5 @@
 export default {
+  // url: 'https://strapi-landing-pages-sobrinho.herokuapp.com/pages/?slug=',
   url: 'http://localhost:1337/pages/?slug=',
   siteName: 'Dev Sobrinho',
   defaultSlug: 'landing-page',
